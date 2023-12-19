508b9cde935c4b9d0acbd61e193562ce26969c5a
Trần Linh Giang <gt07839@gmail.com>
2023-12-19 14:33:08 +0700

    Giang_nop_bai_tap_2

--- ss2_reactJs/bai_tap_1/src/components/Student.js
+++ ss2_reactJs/bai_tap_1/src/components/Student.js
@@ -1,30 +1,36 @@
 import "./student.css";
 const students = [{
+    id:1,
         company: 'Alfreds Futterkiste',
         contact: 'Maria Anders',
         country: 'Germany'
     },
     {
+        id:2,
         company: 'Centro comercial Moctezuma',
         contact: 'Francisco Chang',
         country: 'Mexico'
     },
     {
+        id:3,
         company: 'Ernst Handel',
         contact: 'Roland Mendel',
         country: 'Austria'
     },
     {
+        id:4,
         company: 'Island Trading',
         contact: 'Helen Bennett',
         country: 'UK'
     },
     {
+        id:5,
         company: 'Laughing Bacchus Winecellars',
         contact: 'Yoshi Tannamuri',
         country: 'Canada'
     },
     {
+        id:6,
         company: 'Magazzini Alimentari Riuniti',
         contact: 'Giovanni Rovelli',
         country: 'Italy'
@@ -38,7 +44,7 @@ function Student(){
             <th>country</th>
         </tr>
         {students.map(student => (
-              <tr key={student.company}>
+              <tr key={student.id}>
                 <td>{student.company}</td>
                 <td>{student.contact}</td>
                 <td>{student.country}</td>
